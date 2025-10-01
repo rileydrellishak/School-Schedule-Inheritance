1d7c3254e4791840c7b6015a139f26071709e620
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-01 14:00:56 -0400

    Creates MiddleSchoolStudent class from Student
    Co-authored-by: May <maychanlee@users.noreply.github.com>
    Co-authored-by: FluenceMind <FluenceMind@users.noreply.github.com>

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,18 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, get_transportation=False):
+        
+        super().__init__(name, grade, classes)
+        self.get_transportation = get_transportation
+
+    def display_transportation_message(self):
+        has_message = "has" if self.get_transportation else "doesn't have"
+        return f"{self.name} {has_message} transportation"
+    
+    def summary(self):
+        student_summary = super().summary()
+        transportation_messeage = self.display_transportation_message()
+        
+        return "\n".join((student_summary, transportation_messeage))
